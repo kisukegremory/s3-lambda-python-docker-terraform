36370fd609a125903641f4d19494d5d17b24a2e3
Gustavo de Souza Gonçalves <goncalves.gustavo211@gmail.com>
2024-05-19 11:01:25 -0300

    refac: spliting the lambda functions

--- lambda.tf
+++ lambda.tf
@@ -1,14 +1,14 @@
-data "archive_file" "lambda" {
+data "archive_file" "lambda_vanilla" {
   type        = "zip"
-  source_file = "src/main.py"
-  output_path = "${local.project_name}.zip"
+  source_file = "lambda_vanilla/main.py"
+  output_path = "${local.project_name}-vanilla.zip"
 }
 
-resource "aws_lambda_function" "this" {
+resource "aws_lambda_function" "vanilla" {
   function_name    = "s3-data-processor"
-  filename         = data.archive_file.lambda.output_path
+  filename         = data.archive_file.lambda_vanilla.output_path
   role             = aws_iam_role.this.arn
-  source_code_hash = data.archive_file.lambda.output_base64sha256
+  source_code_hash = data.archive_file.lambda_vanilla.output_base64sha256
   runtime          = "python3.11"
   handler          = "main.lambda_handler"
   timeout          = 10
--- output.tf
+++ output.tf
@@ -1,11 +1,3 @@
 output "bucket_arn" {
   value = aws_s3_bucket.this.arn
 }
-
-output "lambda_arn" {
-  value = aws_lambda_function.this.arn
-}
-
-output "lambda_invoke_arn" {
-  value = aws_lambda_function.this.invoke_arn
-}
