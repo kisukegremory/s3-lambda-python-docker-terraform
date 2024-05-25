8a418865cedd2235f44a5d33c9513f72918107dd
Gustavo de Souza Gonçalves <goncalves.gustavo211@gmail.com>
2024-05-25 16:42:18 -0300

    fix: fixed compatibility with aiobotoce switching from 3.10 -> 3.11

--- src/lambda_on_container/main.py
+++ src/lambda_on_container/main.py
@@ -1,22 +1,23 @@
-from scipy import constants
 import polars as pl
 import s3fs
 
-bucket = "s3-lambda-python-docker-terraform-input-data"
-file_key = "amazon_prime_users.csv"
+# bucket = "s3-lambda-python-docker-terraform-input-data"
+# file_key = "amazon_prime_users.csv"
 fs = s3fs.S3FileSystem()
 
 def lambda_handler(event, context):
+    try:
+        bucket = event['Records'][0]['s3']['bucket']['name']
+        file_key = event['Records'][0]['s3']['object']['key']
+    except:
+        return {
+            'statusCode':404,
+            'errorReason':"unable to find file"
+        }
+    
     df = pl.read_csv(f"s3://{bucket}/{file_key}")
-    df = df.with_columns(pl.lit(constants.Avogadro).alias("scipy_column"))
-    print("Event:")
-    print(event)
-
-    print("Dataframe")
-    print(df)
-
-
-    destination = f"s3://{bucket}/processed/{file_key.split(".")[0]}.parquet"
+    df = df.with_columns(pl.lit(4).alias("scipy_column"))
+    destination = f"s3://{bucket}/processed/{file_key.split('.')[0]}.parquet"
 
     with fs.open(destination, mode="wb") as f:
         df.write_parquet(f)
